6e5aafc0746c43c15d909a53f2d3acfca65673e6
anagarcia791 <amgt2697@gmail.com>
2022-02-03 06:50:40 -0500

    Acorrecion identacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,29 @@
 const cipher = {
-  encode: (offset , string) => {
-      let textoCifrado = "";
-      for (let caracter of string.toUpperCase()){
-          let caracterCode = caracter.charCodeAt(0);
-          if(caracterCode<65 || caracterCode>90){
-              textoCifrado += caracter;
-          }else{
-              let caracterDesplazado = (((caracterCode - 65) + offset) % 26) + 65;
-              textoCifrado += String.fromCharCode(caracterDesplazado);
-          }
-      }return textoCifrado
-  }, 
+    encode: (offset , string) => {
+        let textoCifrado = "";
+        for (let caracter of string.toUpperCase()){
+            let caracterCode = caracter.charCodeAt(0);
+            if(caracterCode<65 || caracterCode>90){
+                textoCifrado += caracter;
+            }else{
+                let caracterDesplazado = (((caracterCode - 65) + offset) % 26) + 65;
+                textoCifrado += String.fromCharCode(caracterDesplazado);
+            }
+        }return textoCifrado
+    }, 
 
-  decode: (offset, string) => {
-      let textoDescifrado = "";
-      for (let caracter2 of string.toUpperCase()){
-          let caracterCode2 = caracter2.charCodeAt(0);
-          if(caracterCode2<65 || caracterCode2>90){
-              textoDescifrado += caracter2;
-          }else{
-              let caracterPosicionOK = (((caracterCode2 + 65) - offset) % 26) + 65;
-              textoDescifrado += String.fromCharCode(caracterPosicionOK);
-          }
-      }return textoDescifrado
-  }
+    decode: (offset, string) => {
+        let textoDescifrado = "";
+        for (let caracter2 of string.toUpperCase()){
+            let caracterCode2 = caracter2.charCodeAt(0);
+            if(caracterCode2<65 || caracterCode2>90){
+                textoDescifrado += caracter2;
+            }else{
+                let caracterPosicionOK = (((caracterCode2 + 65) - offset) % 26) + 65;
+                textoDescifrado += String.fromCharCode(caracterPosicionOK);
+            }
+        }return textoDescifrado
+    }
 
 };
 
